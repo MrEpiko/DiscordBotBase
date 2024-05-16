67a3dd01c675d279af94def7bd3639478f253b3d
MrEpiko <epicplayer73@gmail.com>
2024-05-16 12:42:05 +0200

    Removed runnables from PrivateChannelContext

--- src/main/java/me/mrepiko/discordbotbase/discord/context/channel/PrivateChannelContext.java
+++ src/main/java/me/mrepiko/discordbotbase/discord/context/channel/PrivateChannelContext.java
@@ -10,18 +10,11 @@ import org.jetbrains.annotations.Nullable;
 public class PrivateChannelContext extends ChannelContext {
 
     private final PrivateChannel privateChannel;
-    @Nullable @Getter private final Runnable successfulAction;
-    @Nullable @Getter private final Runnable failedAction;
+
 
     public PrivateChannelContext(PrivateChannel privateChannel) {
-        this(privateChannel, null, null);
-    }
-
-    public PrivateChannelContext(PrivateChannel privateChannel, @Nullable Runnable sucessfulAction, @Nullable Runnable failedAction) {
         super(privateChannel);
         this.privateChannel = privateChannel;
-        this.successfulAction = sucessfulAction;
-        this.failedAction = failedAction;
     }
 
     @Override
